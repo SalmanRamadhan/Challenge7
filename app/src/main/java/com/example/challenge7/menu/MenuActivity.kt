565350ca7670995e1250309685df97034d1afe5d
SalmanRamadhan <msalmanrama98@gmail.com>
2022-09-13 20:33:01 +0700

    Push Initial Branch

--- app/src/main/java/com/example/challenge7/menu/MenuActivity.kt
+++ app/src/main/java/com/example/challenge7/menu/MenuActivity.kt
@@ -2,11 +2,48 @@ package com.example.challenge7.menu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.challenge7.R
+import com.example.challenge7.databinding.ActivityMenuBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MenuActivity : AppCompatActivity() {
+
+
+    private var binding :ActivityMenuBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        actionBar?.hide()
+
+        binding?.btmNavBarMenu?.setOnItemSelectedListener {
+
+            when(it.itemId) {
+                R.id.menu_1 -> {
+                    replaceFragment(ProfileFragment())
+                    true
+                }
+                R.id.menu_2 -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.menu_3 -> {
+                    replaceFragment(HistoryFragment())
+                    true
+                }
+                else -> false
+
+            }
+
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.fragment_container,fragment)
+        fragmentTransaction.commit()
+
     }
 }
