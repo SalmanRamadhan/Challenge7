1132f3d47ee7872a48f27483cf1f57f300feec72
HeriWijaya27 <herimohamaddd@gmail.com>
2022-09-17 15:44:58 +0700

    layouting home fragmen & menu pop up

--- app/src/main/java/com/example/challenge7/menu/HomeFragment.kt
+++ app/src/main/java/com/example/challenge7/menu/HomeFragment.kt
@@ -1,11 +1,11 @@
 package com.example.challenge7.menu
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.PopupMenu
 import com.example.challenge7.R
+import com.example.challenge7.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
@@ -14,7 +14,7 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
