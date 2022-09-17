526bd50ce0163b26f22c56fbcbe8f081b9093a2d
HeriWijaya27 <herimohamaddd@gmail.com>
2022-09-17 11:07:52 +0700

    adding logic landing page and intent to loggin

--- app/src/main/java/com/example/challenge7/SplashScreen.kt
+++ app/src/main/java/com/example/challenge7/SplashScreen.kt
@@ -1,11 +1,26 @@
 package com.example.challenge7
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.challenge7.databinding.ActivitySplashScreenBinding
+import com.example.challenge7.landingpage.LandingPage
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity() {
+    var binding : ActivitySplashScreenBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LandingPage::class.java))
+            finish()
+        }, 3000)
+
     }
 }
