ae4b8c348af9dd655df7d1dab02da22672524e85
SalmanRamadhan <msalmanrama98@gmail.com>
2022-09-22 19:05:17 +0700

    vs COM done minus create history dan sharedprefernce untuk nama dan round

--- app/src/main/java/com/example/challenge7/gameplay/AgainstComActivity.kt
+++ app/src/main/java/com/example/challenge7/gameplay/AgainstComActivity.kt
@@ -1,12 +1,156 @@
 package com.example.challenge7.gameplay
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+
 import com.example.challenge7.R
+import com.example.challenge7.authentication.LoginActivity
+import com.example.challenge7.databinding.ActivityAgainstComBinding
+import com.example.challenge7.gameplay.dialog.ResultDialog
+import com.example.challenge7.menu.MenuActivity
+import kotlin.math.max
 
 class AgainstComActivity : AppCompatActivity() {
+
+    companion object {
+
+        const val BATU = 1
+        const val KERTAS = 2
+        const val GUNTING = 3
+
+    }
+
+    var binding: ActivityAgainstComBinding? = null
+    var roundCounter = 0
+    var maxRound = 3
+    var isPlay = false
+    var comProgress = maxRound
+    var playerProgress = maxRound
+    var playerName = "Salman"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_against_com)
+        binding = ActivityAgainstComBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.ivHome?.setOnClickListener {
+            val backToMenu = Intent(this@AgainstComActivity, MenuActivity::class.java)
+            startActivity(backToMenu)
+        }
+
+        binding?.pbCOM?.progress = maxRound
+        binding?.pbPlayer?.progress = maxRound
+        binding?.pbCOM?.max = maxRound
+        binding?.pbPlayer?.max = maxRound
+
+        binding?.ivRestart?.setOnClickListener {
+            reset()
+        }
+
+        binding?.ivBatu?.setOnClickListener {
+            if (!isPlay) {
+                play(BATU, generateRandomChoice())
+            }
+        }
+
+        binding?.ivKertas?.setOnClickListener {
+            if (!isPlay) {
+                play(KERTAS, generateRandomChoice())
+            }
+        }
+        binding?.ivGunting?.setOnClickListener {
+            if (!isPlay) {
+                play(GUNTING, generateRandomChoice())
+            }
+        }
+
     }
+
+    fun reset() {
+
+
+
+        binding?.pbCOM?.progress = maxRound
+        binding?.pbPlayer?.progress = maxRound
+        binding?.pbCOM?.max = maxRound
+        binding?.pbPlayer?.max = maxRound
+        comProgress = maxRound
+        playerProgress = maxRound
+        roundCounter = 0
+        isPlay = false
+
+    }
+
+    private fun generateRandomChoice(): Int = (1 until 4).random()
+
+    private fun play(p1: Int, p2: Int) {
+
+        roundCounter += 1
+
+        when {
+            p1 == p2 -> {
+                draw()
+            }
+            p1 == BATU && p2 == GUNTING -> {
+                won()
+            }
+            p1 == KERTAS && p2 == BATU -> {
+                won()
+            }
+            p1 == GUNTING && p2 == KERTAS -> {
+                won()
+            }
+            else -> lost()
+        }
+
+    }
+
+    private fun draw() {
+
+        if (roundCounter == maxRound) {
+            showDialogResult()
+        }
+        Toast.makeText(this, "Draw", Toast.LENGTH_SHORT).show()
+
+    }
+
+    private fun won() {
+
+        comProgress -= 1
+        binding?.pbCOM?.progress = comProgress
+        if (roundCounter == maxRound) {
+            showDialogResult()
+        }
+        Toast.makeText(this, "Player Menang", Toast.LENGTH_SHORT).show()
+
+    }
+
+    private fun lost() {
+        playerProgress -= 1
+        binding?.pbPlayer?.progress = playerProgress
+        if (roundCounter == maxRound) {
+            showDialogResult()
+        }
+        Toast.makeText(this, "COM Menang", Toast.LENGTH_SHORT).show()
+
+    }
+
+    private fun showDialogResult() {
+        isPlay = true
+        val dialog = ResultDialog(
+            when {
+                comProgress == playerProgress -> getString(R.string.result_seri)
+                comProgress > playerProgress -> getString(R.string.result_com)
+                comProgress < playerProgress -> playerName
+                else -> ""
+            }
+        )
+        dialog.show(supportFragmentManager, "ResultDialog")
+    }
+
+
 }
